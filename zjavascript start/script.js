ef2ba04111c4fa2c60c97bd9066cd9e0f2727381
j0p21en5 <jaykumarpatel881@gmail.com>
2023-09-07 09:52:02 +0530

    update

---
+++ zjavascript start/script.js
@@ -0,0 +1,7 @@
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click',inputMsg) ;
+function inputMsg() {
+    let name = prompt('Enter any Key ') ;
+    namasteBtn.textContent='Namaste World! : '+ name;
+
+}
